7e80ef8a5b810ed2a86c701d023295d5e12d339e
HanB41 <obagirci@gmail.com>
2021-06-08 22:31:10 +0300

    update category page has been edited

--- screens/categoryupdate.js
+++ screens/categoryupdate.js
@@ -1,53 +1,113 @@
 import React, { useEffect, useState } from "react";
+
 import { TouchableOpacity } from 'react-native';
+
 import { View, Text, Button } from 'react-native';
 
+import { useRoute } from '@react-navigation/native';
+
+ 
+
 import { Input } from 'react-native-elements/dist/input/Input';
+
 import { useNavigation } from '@react-navigation/native';
+
 import config from '../env/config'
+
+ 
 
 export default function CategoryUpdate() {
 
-  const [categoryname, setCategoryName] = useState([])
-  const [id, setID] = useState([])
+ 
+
+  const route = useRoute();
+
+ 
+
+  const [name, setName] = useState([])
+
+  const [id, setId] = useState([])
+
   const [description, setDescription] = useState([])
+
+ 
 
   const navigation = useNavigation();
 
+ 
+
   const update = async () => {
-        await fetch(config.apiurl + 'api/categories/'+id,{
+
+        await fetch(config.apiurl + 'api/categories/'+route.params.id,{
+
           method: 'PUT',
+
           headers: {'Content-Type': 'application/json'},
+
           body: JSON.stringify({
-            categoryname,
+
+            name,
+
             description
+
           })
+
         });
-        navigation.navigate('CategoryUpdate')
+
+        alert('Category has been updated !');
+
       }
 
+ 
 
+ 
+
+ 
 
   return (
+
     <View>
 
+ 
+
       <Input
+
+ 
 
         onChangeText={value => setDescription(value)}
+
         placeholder='Description'
+
+        value = {description}
+
       />
+
+ 
 
       <Input
 
-        onChangeText={value => setCategoryName(value)}
+ 
+
+        onChangeText={value => setName(value)}
+
         placeholder='Name'
+
+        value = {name}
+
       />
 
+ 
+
       <TouchableOpacity onPress={() => update()} style={{ flex: 1, backgroundColor: '#EF9630', textAlign: 'center', padding: 20, borderRadius: 20, marginTop: 10 }}
+
       > UPDATE
+
       </TouchableOpacity>
+
+ 
 
     </View>
 
-  );
+  )
+
 }
